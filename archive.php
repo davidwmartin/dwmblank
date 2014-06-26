af101b49c0599ac5a72eb284977a24dd99365e1a
David Martin <david.w.martin@me.com>
2014-06-26 15:58:31 -0700

    fixed archives template

--- archive.php
+++ archive.php
@@ -1,13 +1,30 @@
+<?php if ( have_posts() ) : ?>
 <header>
 	<h1><?php
 		if ( is_day() ) { printf( __( 'Daily Archives: %s', 'dwmblank' ), get_the_time( get_option( 'date_format' ) ) ); }
 		elseif ( is_month() ) { printf( __( 'Monthly Archives: %s', 'dwmblank' ), get_the_time( 'F Y' ) ); }
 		elseif ( is_year() ) { printf( __( 'Yearly Archives: %s', 'dwmblank' ), get_the_time( 'Y' ) ); }
+		elseif ( is_category()) {
+			_e( "Category Archive for: ", "dwmblank").single_cat_title();
+		}
+		elseif ( is_tag()) {
+			_e( "Tag Archive for: ", "dwmblank").single_cat_title();
+		}
+		elseif ( is_author()) {
+			_e("Author Archive", "dwmblank");
+		}
 		else { _e( 'Archives', 'dwmblank' ); }
-	?></h1> 
+	?></h1>
 </header>
+
 <?php while ( have_posts() ) : the_post(); ?>
 
-	<?php get_template_part( 'content', get_post_format() ); ?>
+	<?php get_template_part( 'templates/content', get_post_format() ); ?>
 
 <?php endwhile; ?>
+
+<?php else: ?>
+
+	<h2><?php _e('Nothing Found','dwmblank'); ?></h2>
+
+<?php endif; ?>
