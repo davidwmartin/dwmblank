7fc4caadfa1802f5258ea28ba2da1ba966d012d7
David Martin <david.w.martin@me.com>
2016-01-26 14:07:32 -0800

    updated jquery dependency to latest (2.2.0)

--- lib/scripts.php
+++ lib/scripts.php
@@ -3,7 +3,7 @@
 	if (!is_admin()) {
 		function my_jquery_enqueue() {
 		   wp_deregister_script('jquery');
-		   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js", false, null);
+		   wp_register_script('jquery', "http" . ($_SERVER['SERVER_PORT'] == 443 ? "s" : "") . "://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js", false, null);
 		   wp_enqueue_script('jquery');
 		};
 		add_action("wp_enqueue_scripts", "my_jquery_enqueue", 11);
